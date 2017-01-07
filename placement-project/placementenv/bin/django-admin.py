c5d40c1d027c20b12520ae64ce5b232d26cc152f
Mounika <Madelamounica@gmail.com>
2017-01-07 22:48:18 +0000

    Adding virtual environment

---
+++ placement-project/placementenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kiran/project/BVRIT-ModuleVII-Team-11/placementproj/placementenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
