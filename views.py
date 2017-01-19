64639963434d1ec9652ed4bf732e81d49b8f41d5
Srikari Surampudi <srikari.surampudi@gmail.com>
2017-01-19 10:37:28 +0530

    Update views.py

--- views.py
+++ views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Login</h1><form>First name:<br><input type='text' name='firstname' ><br>Last name:<br><input type='text' name='lastname'><br><br><input type='submit' value='Submit'></form> ")
+    return HttpResponse("<h1>Login</h1><form>username:<br><input type='text' name='username' ><br>password:<br><input type='password' name='password'><br><br><input type='submit' value='Submit'></form> ")
