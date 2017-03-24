ec8a1fa3c2816725adf72a58fe0b3ad697216768
Mounika <Madelamounica@gmail.com>
2017-03-24 14:15:03 +0000

    Adding faculty page

--- faculty-project/bvrithsite/accounts/views.py
+++ faculty-project/bvrithsite/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+  
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+    )
+
+def faculty(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+  
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'faculty.html',
+    )
