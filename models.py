5ed09cbaaf67f7db805db1fdac6c06a3cbb9893c
Srikari Surampudi <srikari.surampudi@gmail.com>
2017-03-25 11:21:49 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class PersonalInfo(models.Model):
+   rollno = models.CharField(max_length=30)
+   firstname = models.CharField(max_length=30)
+   lastname = models.CharField(max_length=30)
+   email = models.CharField(max_length=30)
+   phonenumber = models.CharField(max_length=10)
+   dob = models.DateTimeField(blank=True, null=True)
+   address = models.CharField(max_length=70)
+
+   def __str__(self):
+      return self.rollno
+
+
 class AcademicInfo(models.Model):
    yearofjoining = models.DateTimeField(blank=True, null=True)
    currentsem = models.CharField(max_length=8)
